9331f0a18e1c205cb37cdd7092de07926ae4e546
Patrick Decat <patrick.decat@fr.clara.net>
2019-11-05 02:52:54 +0100

    TER-35: add get_stack_from_config_path

--- test_helpers.py
+++ test_helpers.py
@@ -32,6 +32,8 @@ def test_get_stack_dir(tmp_working_dir):
 
 def test_get_stack_config_filename():
     for account, environment, region, stack, expected in [
+        ("*", "global", None, "*", "*_global_*.yml",),
+        ("*", "*", "*", "*", "*_*_*_*.yml",),
         (
             "testaccount",
             "global",
@@ -73,3 +75,26 @@ def test_get_stack_config_path(tmp_working_dir_empty_conf):
         assert tfwrapper.get_stack_config_path(
             conf_dir, account, environment, region, stack
         ) == str(expected)
+
+
+def test_get_stack_from_config_path(tmp_working_dir_empty_conf):
+    conf_dir = tmp_working_dir_empty_conf["conf_dir"]
+    for config_path, expected in [
+        (
+            conf_dir / "testaccount_global_teststack.yml",
+            ("testaccount", "global", None, "teststack"),
+        ),
+        (
+            conf_dir / "testaccount_testenvironment_testregion_teststack.yml",
+            ("testaccount", "testenvironment", "testregion", "teststack"),
+        ),
+        (
+            "conf/testaccount_global_teststack.yml",
+            ("testaccount", "global", None, "teststack"),
+        ),
+        (
+            "conf/testaccount_testenvironment_testregion_teststack.yml",
+            ("testaccount", "testenvironment", "testregion", "teststack"),
+        ),
+    ]:
+        assert tfwrapper.get_stack_from_config_path(str(config_path)) == expected
