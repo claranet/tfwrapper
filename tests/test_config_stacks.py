4ae3090a8ed69542368f8066e06828abfbcca02b
Patrick Decat <patrick.decat@fr.clara.net>
2025-09-09 16:20:43 +0200

    fix: tolerate underscore character in stack names

--- tests/test_config_stacks.py
+++ tests/test_config_stacks.py
@@ -59,6 +59,60 @@ def test_aws(tmp_working_dir_empty_conf):
     assert parsed_stack_config == expected_config
 
 
+def test_stack_with_underscores(tmp_working_dir_empty_conf):
+    paths = tmp_working_dir_empty_conf
+    stack_config_b = paths["conf_dir"] / "testaccount_testenvironment_testregion_teststack_with_underscores.yml"
+    paths["state_conf"].write_text(
+        textwrap.dedent(
+            """
+            ---
+            aws:
+                general:
+                    account: '12345678910'
+                    region: eu-west-1
+                credentials:
+                    profile: terraform-states-profile
+
+            backend_parameters:
+                state_snaphot: "false"
+            """
+        )
+    )
+    stack_config_b.write_text(
+        textwrap.dedent(
+            """
+            ---
+            aws:
+              general:
+                account: &aws_account '12345678910'
+                region: &aws_region eu-west-1
+              credentials:
+                profile: account-alias
+            terraform:
+              vars:
+                client_name: claranet
+                aws_account: *aws_account
+                aws_region: *aws_region
+            """
+        )
+    )
+
+    account = "testaccount"
+    environment = "testenvironment"
+    region = "testregion"
+    stack = "teststack_with_underscores"
+
+    stack_config_file = tfwrapper.get_stack_config_path(paths["conf_dir"], account, environment, region, stack)
+    parsed_stack_config = tfwrapper.load_stack_config_from_file(stack_config_file)
+
+    expected_config = {
+        "aws": {"general": {"account": "12345678910", "region": "eu-west-1"}, "credentials": {"profile": "account-alias"}},
+        "terraform": {"vars": {"client_name": "claranet", "aws_account": "12345678910", "aws_region": "eu-west-1"}},
+    }
+
+    assert parsed_stack_config == expected_config
+
+
 def test_azure_user(tmp_working_dir_empty_conf):
     paths = tmp_working_dir_empty_conf
     stack_config_b = paths["conf_dir"] / "testaccount_testenvironment_testregion_teststack.yml"
